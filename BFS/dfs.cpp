60931e8d4baa5b6f6c8e751fc318721065367023
Gourav Bhardwaj <gouravbhardwaj3007@gmail.com>
2023-03-31 15:29:33 -0700

    Added Topological Sort to dfs.cpp

--- BFS/dfs.cpp
+++ BFS/dfs.cpp
@@ -1,9 +1,11 @@
-// O(|V|^2) as it is done with a matrix, otherwise O(|V|+|E|) with adjacency list
+// O(|V|^2) for DFS and Topological Sort as it is done with a matrix, otherwise O(|V|+|E|) with adjacency list
 
 #include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
+//directed graph
 class Graph {
 private:
 	vector<vector<int>> adjMatrix;
@@ -11,6 +13,7 @@ private:
 	vector<int> discovered;						//when we first discover a vertex
 	vector<int> checked;						//when all edges of a vertex are explored
 	vector<int> parent;
+	vector<int> topSorted;
 
 	int step = 1;
 
@@ -21,16 +24,22 @@ public:
 		discovered.resize(numVertices, -1);
 		checked.resize(numVertices, -1);
 		parent.resize(numVertices, -1);
+		topSorted.resize(numVertices, -1);
 	}
 
 	void addEdge(int u, int v) {
-		adjMatrix[u][v] = 1;
-		adjMatrix[v][u] = 1;					//undirected graph
+		adjMatrix[u][v] = 1;					//directed graph
 	}
 
-	//only works if G is connected
+	//graph doesn't have to connected
 	//use for each vertex loop if G is not connected
-	void DFS(int s) {
+	void DFS() {
+		for (int i = 0; i < adjMatrix.size(); i++) {
+			DFSRecusrive(i);
+		}
+	}
+
+	void DFSRecusrive(int s) {
 		if (color[s] == "white") {
 			cout << s << " ";
 			color[s] = "grey";
@@ -39,12 +48,13 @@ public:
 				if ((adjMatrix[s][v] == 1) && (color[v] == "white")) {
 					parent[v] = s;
 					step++;
-					DFS(v);
+					DFSRecusrive(v);
 				}
 			}
 			color[s] = "black";
 			step++;
 			checked[s] = step;
+			topologicalSort(s);
 		}
 	}
 	
@@ -52,6 +62,17 @@ public:
 		cout << endl;
 		for (int i = 0; i < adjMatrix.size(); i++) {
 			cout << "Vertex " << i << " with Parent " << parent[i] << " : Discovered in Step: " << discovered[i] << " Checked in Step: " << checked[i] << endl;
+		}
+	}
+
+	void topologicalSort(int s) {
+		topSorted.insert(topSorted.begin(), s);
+	}
+
+	void printTopSorted() {
+		cout << "Topologically sorted list is: ";
+		for (int i = 0; i < adjMatrix.size(); i++) {
+			cout << topSorted[i] << " ";
 		}
 	}
 };
@@ -65,9 +86,11 @@ int main() {
 	G.addEdge(3, 4);
 	G.addEdge(3,5);
 
-	G.DFS(5);
+	G.DFS();
 
 	G.printDandC();
+
+	G.printTopSorted();
 
 	return 0;
 }
